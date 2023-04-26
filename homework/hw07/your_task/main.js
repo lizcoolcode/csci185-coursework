3e132927e556abc84ec9d710fc6a6d0c33cd3052
Liz <chumphr1@unca.edu>
2023-04-26 18:39:47 -0400

    hw7

--- homework/hw07/your_task/main.js
+++ homework/hw07/your_task/main.js
@@ -13,32 +13,63 @@ function search (ev) {
 }
 
 async function getTracks (term) {
-    console.log(`
-        get tracks from spotify based on the search term
-        "${term}" and load them into the #tracks section 
-        of the DOM...`);
+
+    document.querySelector('#tracks').innerHTML = '';
+
+    const url = `https://www.apitutor.org/spotify/simple/v1/search?type=track&q=${term}`;
+    const data = await fetch(url).then(response => response.json());
+    console.log(data);
+    for (let i = 0; i < 5; i++) {
+        const track = data[i];
+        const template = `
+            <section class="track-item preview" onclick="loadTrack()">
+            <img src="${track.album.image_url}">
+            <i class="fas play-track fa-play" aria-hidden="true"></i>
+            <div class="label">
+                <h2>${track.name}</h2>
+                <p>
+                    ${track.artist.name}
+                </p>
+            </div>
+            </section>
+        `;
+        document.querySelector('#tracks').innerHTML += template;
+    }
 }
 
 async function getAlbums (term) {
-    console.log(`
-        get albums from spotify based on the search term
-        "${term}" and load them into the #albums section 
-        of the DOM...`);
+    const url = `https://www.apitutor.org/spotify/simple/v1/search?type=album&q=${term}`;
+    const data = await fetch(url).then(response => response.json());
+    console.log(data);
 }
 
 async function getArtist (term) {
-    console.log(`
-        get artists from spotify based on the search term
-        "${term}" and load the first artist into the #artist section 
-        of the DOM...`);
+    const url = `https://www.apitutor.org/spotify/simple/v1/search?type=artist&q=${term}`;
+    const data = await fetch(url).then(response => response.json());
+    console.log(data);
+    const artist = data[0];
+    const template = `
+        <section class="artist-card" id="3Nrfpe0tUJi4K4DXYWgMUX">
+        <div>
+            <img src="${artist.image_url}">
+            <h2>${artist.name}</h2>
+            <div class="footer">
+                <a href="${artist.spotify_url}" target="_blank">
+                    view on spotify
+                </a>
+            </div>
+        </div>
+    </section>
+    `;
+    document.querySelector('#artist').innerHTML = template;
 };
 
 
 document.querySelector('#search').onkeyup = function (ev) {
     // Number 13 is the "Enter" key on the keyboard
     console.log(ev.keyCode);
-    if (ev.keyCode === 13) {
+    //if (ev.keyCode === 13) {
         ev.preventDefault();
         search();
-    }
+    //}
 }
